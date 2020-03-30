7e853994f69c3a4c9e82eaf2f6e608341ce58066
Tom Burdick <thomas.burdick@gmail.com>
2020-03-30 17:29:42 -0500

    Fix LED exmaple, requires a imxrt-rt on a branch

--- examples/src/led.rs
+++ examples/src/led.rs
@@ -8,11 +8,10 @@
 extern crate panic_semihosting;
 extern crate cortex_m_semihosting;
 
-use bsp::rt::entry;
 use imxrt1060evk_bsp as bsp;
-use cortex_m_semihosting::{debug, hio};
-use core::fmt::Write;
+use bsp::rt::entry;
 use bsp::hal::gpio::IntoGpio;
+use cortex_m::asm::wfi;
 
 
 use embedded_hal::digital::v2::ToggleableOutputPin;
@@ -21,11 +20,8 @@ use embedded_hal::digital::v2::ToggleableOutputPin;
 fn main() -> ! {
     let mut peripherals = bsp::Peripherals::take().unwrap();
     let mut led = bsp::configure_led(&mut peripherals.gpr, peripherals.pins.d4.alt5());
-    let mut stdout = hio::hstdout().unwrap();
 
-    write!(stdout, "init done, looping").unwrap();
     loop {
-        write!(stdout, "toggling").unwrap();
         led.toggle().unwrap();
         for i in 0..1000 {
             core::sync::atomic::spin_loop_hint();
