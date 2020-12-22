29acab4e6b33905427a7d6a0b331654a8564dc47
Tom Burdick <thomas.burdick@gmail.com>
2020-12-21 21:49:20 -0600

    Update led pin config to imxrt-iomuxc

--- imxrt1060evk-pins/src/lib.rs
+++ imxrt1060evk-pins/src/lib.rs
@@ -3,6 +3,51 @@
 //! `imxrt1060evk-pins` provides types and a struct containing the set of Pins
 //! the board exposes to other devices and IO connectors.
 //!
+//! ## Arduino Connector and Alternative Functions
+//!
+//! The sparse table below describes the Arduino header pins, the pad ID, and
+//! some of the notable alternative functionalities for each pin. We add entries
+//! to the table as we add capabilities to the underlying HAL crate.
+//! Contributions to complete this table are welcome! If a pad's alternatives
+//! are not listed here, consult the iMXRT1060 reference manual.
+//!
+//! | Pin   | Pad ID   |  Alt0    |  Alt1        |  Alt2        |  Alt3     |  Alt4        |  Alt5            |  Alt6        |  Alt7   |  Alt8   |  Alt9   |
+//! | ----- | -------- | -------- | ------------ | ------------ | --------- | ------------ | ---------------- | ------------ | ------- | ------- | ------- |
+//! |  D0   |`AD_B1_07`|          |              |              |           |              |                  |              |         |         |         |
+//! |  D1   |`AD_B1_06`|          |              |              |           |              |                  |              |         |         |         |
+//! |  D2   |`AD_B0_11`|          |`FlexPWM1_3_B`|              |           |              |                  |              |         |         |         |
+//! |  D3   |`AD_B1_08`|          |              |              |           |              |                  |              |         |         |         |
+//! |  D4   |`AD_B0_09`|          |              |              |           |              |                  |              |         |         |         |
+//! |  D5   |`AD_B0_10`|          |`FlexPWM1_3_A`|              |           |              |                  |              |         |         |         |
+//! |  D6   |`AD_B1_02`|          |              |              |           |              |                  |              |         |         |         |
+//! |  D7   |`AD_B1_03`|          |              |              |           |              |                  |              |         |         |         |
+//! |  D8   |`AD_B0_03`|          |              |              |           |              |                  |              |         |         |         |
+//! |  D9   |`AD_B0_02`|          |              |              |           |              |                  |              |         |         |         |
+//! |  D10  |`SD_B0_01`|          |`FlexPWM1_0_B`|              |           |              |                  |              |         |         |         |
+//! |  D11  |`SD_B0_02`|          |              |              |           |              |                  |              |         |         |         |
+//! |  D12  |`SD_B0_03`|          |              |              |           |              |                  |              |         |         |         |
+//! |  D13  |`SD_B0_00`|          |`FlexPWM1_0_A`|              |           |              |`GPIO2_3`         |              |         |         |         |
+//! |  D14  |`AD_B1_01`|          |              |              |           |              |                  |              |         |         |         |
+//! |  D15  |`AD_B1_00`|          |              |              |           |              |                  |              |         |         |         |
+//! |  A0   |`AD_B1_10`|          |              |              |           |              |                  |              |         |         |         |
+//! |  A1   |`AD_B1_11`|          |              |              |           |              |                  |              |         |         |         |
+//! |  A2   |`AD_B1_04`|          |              |              |           |              |                  |              |         |         |         |
+//! |  A3   |`AD_B1_05`|          |              |              |           |              |                  |              |         |         |         |
+//! |  A4   |`AD_B1_01`|          |              |              |           |              |                  |              |         |         |         |
+//! |  A5   |`AD_B1_00`|          |              |              |           |              |                  |              |         |         |         |
+//!
+//!
+//! ## UART1 - connected to OpenSDA debugger
+//! | Pin      | Pad ID   |  Alt2        |
+//! | -----    | -------- | ------------ |
+//! |  UART_TX |`AD_B0_12`|  `UART1_TX`  |
+//! |  UART_RX |`AD_B0_13`|  `UART1_RX`  |
+//!
+//! ## LED - Connected to Arduino Header and on board LED
+//! | Pin   | Pad ID   | Alt5  |
+//! | ----- | -------- | ----- |
+//! | D4    |`AD_B0_09`|`GPIO` |
+//!
 //! Not every pin is connected to the SoC.
 
 mod iomuxc {
@@ -12,30 +57,50 @@ mod iomuxc {
 
 use crate::iomuxc::{ad_b0::*, ad_b1::*, sd_b0::*, ErasedPad};
 
-type D0 = AD_B1_07;
-type D1 = AD_B1_06;
-type D2 = AD_B0_11;
-type D3 = AD_B1_08;
-type D4 = AD_B0_09;
-type D5 = AD_B0_10;
-type D6 = AD_B1_02;
-type D7 = AD_B1_03;
-type D8 = AD_B0_03;
-type D9 = AD_B0_02;
-type D10 = SD_B0_01;
-type D11 = SD_B0_02;
-type D12 = SD_B0_03;
-type D13 = SD_B0_00;
-type D14 = AD_B1_01;
-type D15 = AD_B1_00;
-type A0 = AD_B1_10;
-type A1 = AD_B1_11;
-type A2 = AD_B1_04;
-type A3 = AD_B1_05;
+/// Arduino D0
+pub type D0 = AD_B1_07;
+/// Arduino D1
+pub type D1 = AD_B1_06;
+/// Arduino D2
+pub type D2 = AD_B0_11;
+/// Arduino D3
+pub type D3 = AD_B1_08;
+/// Arduino D4
+pub type D4 = AD_B0_09;
+/// Arduino D5
+pub type D5 = AD_B0_10;
+/// Arduino D6
+pub type D6 = AD_B1_02;
+/// Arduino D7
+pub type D7 = AD_B1_03;
+/// Arduino D8
+pub type D8 = AD_B0_03;
+/// Arduino D9
+pub type D9 = AD_B0_02;
+/// Arduino D10
+pub type D10 = SD_B0_01;
+/// Arduino D11
+pub type D11 = SD_B0_02;
+/// Arduino D12
+pub type D12 = SD_B0_03;
+/// Arduino D13
+pub type D13 = SD_B0_00;
+/// Arduino D14
+pub type D14 = AD_B1_01;
+/// Arduino D15
+pub type D15 = AD_B1_00;
+/// Arduino A0
+pub type A0 = AD_B1_10;
+/// Arduino A1
+pub type A1 = AD_B1_11;
+/// Arduino A2
+pub type A2 = AD_B1_04;
+/// Arduino A3
+pub type A3 = AD_B1_05;
 
-/// UART_TX pin connected to onboard LPC4322 DAP-LINK
+/// UartTx pin connected to onboard LPC4322 DAP-LINK
 type UartTx = AD_B0_12;
-/// UART_RX pin connected to onboard LPC4322 DAP-LINK
+/// UartRx pin connected to onboard LPC4322 DAP-LINK
 type UartRx = AD_B0_13;
 
 /// Type-erased pins
@@ -89,10 +154,8 @@ pub struct Pins {
     pub a2: A2,
     /// Arduino A3
     pub a3: A3,
-
     // Built-in debug probe uart tx
     pub uart_tx: UartTx,
-
     // Built-in debug probe uart rx
     pub uart_rx: UartRx,
 }
@@ -120,7 +183,6 @@ pub const fn into_pins(iomuxc: crate::iomuxc::Pads) -> Pins {
         a1: iomuxc.ad_b1.p11,
         a2: iomuxc.ad_b1.p04,
         a3: iomuxc.ad_b1.p05,
-
         uart_tx: iomuxc.ad_b0.p12,
         uart_rx: iomuxc.ad_b0.p13,
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 //! A Rust board support package (BSP) for the MIMXRT1060-EVK
 //!
 //! As of this writing, the BSP is very primitive. It exposes
-//! only the LED, and it configures facilities for logging over UART.
+//! only the LED, and it configures facilities for logging over UART to
+//! the on-board LPC4432 which acts as a debug probe.
+//!
 //! Otherwise, it simply forwards components from the HAL for your
 //! own usage. This will be addressed as the HAL becomes more developed.
 //!
@@ -28,51 +30,6 @@
 //! For simplicity, there may be other choice APIs from either crate that
 //! are re-exported in the BSP namespace.
 //!
-//! ## Arduino Connector and Alternative Functions
-//!
-//! The sparse table below describes the Arduino header pins, the pad ID, and
-//! some of the notable alternative functionalities for each pin. We add entries
-//! to the table as we add capabilities to the underlying HAL crate.
-//! Contributions to complete this table are welcome! If a pad's alternatives
-//! are not listed here, consult the iMXRT1060 reference manual.
-//!
-//! | Pin   | Pad ID   |  Alt0    |  Alt1        |  Alt2        |  Alt3     |  Alt4        |  Alt5            |  Alt6        |  Alt7   |  Alt8   |  Alt9   |
-//! | ----- | -------- | -------- | ------------ | ------------ | --------- | ------------ | ---------------- | ------------ | ------- | ------- | ------- |
-//! |  D0   |`AD_B1_07`|          |              |              |           |              |                  |              |         |         |         |
-//! |  D1   |`AD_B1_06`|          |              |              |           |              |                  |              |         |         |         |
-//! |  D2   |`AD_B0_11`|          |`FlexPWM1_3_B`|              |           |              |                  |              |         |         |         |
-//! |  D3   |`AD_B1_08`|          |              |              |           |              |                  |              |         |         |         |
-//! |  D4   |`AD_B0_09`|          |              |              |           |              |                  |              |         |         |         |
-//! |  D5   |`AD_B0_10`|          |`FlexPWM1_3_A`|              |           |              |                  |              |         |         |         |
-//! |  D6   |`AD_B1_02`|          |              |              |           |              |                  |              |         |         |         |
-//! |  D7   |`AD_B1_03`|          |              |              |           |              |                  |              |         |         |         |
-//! |  D8   |`AD_B0_03`|          |              |              |           |              |                  |              |         |         |         |
-//! |  D9   |`AD_B0_02`|          |              |              |           |              |                  |              |         |         |         |
-//! |  D10  |`SD_B0_01`|          |`FlexPWM1_0_B`|              |           |              |                  |              |         |         |         |
-//! |  D11  |`SD_B0_02`|          |              |              |           |              |                  |              |         |         |         |
-//! |  D12  |`SD_B0_03`|          |              |              |           |              |                  |              |         |         |         |
-//! |  D13  |`SD_B0_00`|          |`FlexPWM1_0_A`|              |           |              |`GPIO2_3`         |              |         |         |         |
-//! |  D14  |`AD_B1_01`|          |              |              |           |              |                  |              |         |         |         |
-//! |  D15  |`AD_B1_00`|          |              |              |           |              |                  |              |         |         |         |
-//! |  A0   |`AD_B1_10`|          |              |              |           |              |                  |              |         |         |         |
-//! |  A1   |`AD_B1_11`|          |              |              |           |              |                  |              |         |         |         |
-//! |  A2   |`AD_B1_04`|          |              |              |           |              |                  |              |         |         |         |
-//! |  A3   |`AD_B1_05`|          |              |              |           |              |                  |              |         |         |         |
-//! |  A4   |`AD_B1_01`|          |              |              |           |              |                  |              |         |         |         |
-//! |  A5   |`AD_B1_00`|          |              |              |           |              |                  |              |         |         |         |
-//!
-//!
-//! ## UART1 - connected to OpenSDA debugger
-//! | Pin      | Pad ID   |  Alt2        |
-//! | -----    | -------- | ------------ |
-//! |  UART_TX |`AD_B0_12`|  `UART1_TX`  |
-//! |  UART_RX |`AD_B0_13`|  `UART1_RX`  |
-//!
-//! ## LED - Connected to Arduino Header and on board LED
-//! | Pin   | Pad ID   | Alt5  |
-//! | ----- | -------- | ----- |
-//! | D4    |`AD_B0_09`|`GPIO` |
-//!
 //! ## Examples
 //!
 //! See the `examples` directory for build-able, run-able
@@ -88,187 +45,38 @@
 
 #![no_std]
 
-// Needed generated boot configuration data
-//include!(concat!(env!("OUT_DIR"), "/fcb.rs"));
+pub use imxrt1060evk_pins as pins;
 
+#[cfg(feature = "systick")]
+mod systick;
+#[cfg(feature = "systick")]
+pub use systick::SysTick;
 
-/// Arduino pins that do not yet have a function
+pub use hal::ral::interrupt;
+// `rtic` expects these in the root.
+#[doc(hidden)]
+#[cfg(feature = "rtic")]
+pub use hal::ral::{interrupt as Interrupt, NVIC_PRIO_BITS};
+
+pub use hal::Peripherals;
+pub use imxrt_hal as hal;
+
+/// The LED
 ///
-/// Pin 13 can be used for several things; one common usage is for the on-board LED.
-pub struct Pins {
-    pub d0: hal::iomuxc::gpio::GPIO_AD_B1_07<hal::iomuxc::Alt5>,
-    pub d1: hal::iomuxc::gpio::GPIO_AD_B1_06<hal::iomuxc::Alt5>,
-    pub d2: hal::iomuxc::gpio::GPIO_AD_B0_11<hal::iomuxc::Alt0>,
-    pub d3: hal::iomuxc::gpio::GPIO_AD_B1_08<hal::iomuxc::Alt5>,
-    pub d4: hal::iomuxc::gpio::GPIO_AD_B0_09<hal::iomuxc::Alt0>,
-    pub d5: hal::iomuxc::gpio::GPIO_AD_B0_10<hal::iomuxc::Alt0>,
-    pub d6: hal::iomuxc::gpio::GPIO_AD_B1_02<hal::iomuxc::Alt5>,
-    pub d7: hal::iomuxc::gpio::GPIO_AD_B1_03<hal::iomuxc::Alt5>,
-    pub d8: hal::iomuxc::gpio::GPIO_AD_B0_03<hal::iomuxc::Alt5>,
-    pub d9: hal::iomuxc::gpio::GPIO_AD_B0_02<hal::iomuxc::Alt5>,
-    pub d10: hal::iomuxc::gpio::GPIO_SD_B0_01<hal::iomuxc::Alt5>,
-    pub d11: hal::iomuxc::gpio::GPIO_SD_B0_02<hal::iomuxc::Alt5>,
-    pub d12: hal::iomuxc::gpio::GPIO_SD_B0_03<hal::iomuxc::Alt5>,
-    pub d13: hal::iomuxc::gpio::GPIO_SD_B0_00<hal::iomuxc::Alt5>,
-    pub d14: hal::iomuxc::gpio::GPIO_AD_B1_01<hal::iomuxc::Alt5>,
-    pub d15: hal::iomuxc::gpio::GPIO_AD_B1_00<hal::iomuxc::Alt5>,
-    pub a0: hal::iomuxc::gpio::GPIO_AD_B1_10<hal::iomuxc::Alt5>,
-    pub a1: hal::iomuxc::gpio::GPIO_AD_B1_11<hal::iomuxc::Alt5>,
-    pub a2: hal::iomuxc::gpio::GPIO_AD_B1_04<hal::iomuxc::Alt5>,
-    pub a3: hal::iomuxc::gpio::GPIO_AD_B1_05<hal::iomuxc::Alt5>,
+/// See [`configure_led`](fn.configure_led.html) to prepare the LED.
+pub type LED = hal::gpio::GPIO<pins::D4, hal::gpio::Output>;
 
-    pub uart_tx: hal::iomuxc::gpio::GPIO_AD_B0_12<hal::iomuxc::Alt2>,
-    pub uart_rx: hal::iomuxc::gpio::GPIO_AD_B0_13<hal::iomuxc::Alt2>,
-}
-
-/// All peripherals available on the Teensy4
-pub struct Peripherals {
-    /// Clock control module (forwarded from the HAL)
-    pub ccm: hal::ccm::CCM,
-    /// PIT timers (forwarded from the HAL)
-    pub pit: hal::pit::UnclockedPIT,
-    /// DCDC converters
-    pub dcdc: hal::dcdc::DCDC,
-    /// PWM1 controller
-    pub pwm1: hal::pwm::Unclocked<hal::pwm::module::_1>,
-    /// PWM2 controller
-    pub pwm2: hal::pwm::Unclocked<hal::pwm::module::_2>,
-    /// PWM3 controller
-    pub pwm3: hal::pwm::Unclocked<hal::pwm::module::_3>,
-    /// PWM4 controller
-    pub pwm4: hal::pwm::Unclocked<hal::pwm::module::_4>,
-    /// Pins
-    pub pins: Pins,
-    /// Unclocked I2C peripherals
-    pub i2c: hal::i2c::Unclocked,
-    /// Unclocked SPI peripherals
-    pub spi: hal::spi::Unclocked,
-    /// Unclocked UART peripherals
-    pub uart: hal::uart::Unclocked,
-    /// General purpose registers, used when configuring GPIO pins.
-    pub gpr: hal::iomuxc::GPR,
-}
-
-/// SYSTICK external clock frequency
+/// Configure the board's LED
 ///
-/// See Section 12.3.2.1 of the reference manual. The note
-/// explains that the 24MHz clock is divided down to 100KHz
-/// before reaching SYSTICK.
-const SYSTICK_EXT_FREQ: u32 = 100_000;
-
-impl Peripherals {
-    /// Instantiate the system peripherals. This may only be called once!
-    pub fn take() -> Option<Self> {
-        let mut cp = cortex_m::Peripherals::take()?;
-        cp.SCB.enable_icache();
-        cp.SCB.enable_dcache(&mut cp.CPUID);
-        cp.SYST.disable_counter();
-        cp.SYST.set_clock_source(cortex_m::peripheral::syst::SystClkSource::External);
-        cp.SYST.set_reload((SYSTICK_EXT_FREQ / 1000) - 1);
-        cp.SYST.clear_current();
-        cp.SYST.enable_counter();
-        cp.SYST.enable_interrupt();
-        let p = hal::Peripherals::take()?;
-        Some(Peripherals::new(p))
-    }
-
-    fn new(p: hal::Peripherals) -> Peripherals {
-        Peripherals {
-            ccm: p.ccm,
-            pit: p.pit,
-            dcdc: p.dcdc,
-            pwm1: p.pwm1,
-            pwm2: p.pwm2,
-            pwm3: p.pwm3,
-            pwm4: p.pwm4,
-            pins: Pins {
-                d0: p.iomuxc.gpio_ad_b1_07,
-                d1: p.iomuxc.gpio_ad_b1_06,
-                d2: p.iomuxc.gpio_ad_b0_11,
-                d3: p.iomuxc.gpio_ad_b1_08,
-                d4: p.iomuxc.gpio_ad_b0_09,
-                d5: p.iomuxc.gpio_ad_b0_10,
-                d6: p.iomuxc.gpio_ad_b1_02,
-                d7: p.iomuxc.gpio_ad_b1_03,
-                d8: p.iomuxc.gpio_ad_b0_03,
-                d9: p.iomuxc.gpio_ad_b0_02,
-                d10: p.iomuxc.gpio_sd_b0_01,
-                d11: p.iomuxc.gpio_sd_b0_02,
-                d12: p.iomuxc.gpio_sd_b0_03,
-                d13: p.iomuxc.gpio_sd_b0_00,
-                d14: p.iomuxc.gpio_ad_b1_01,
-                d15: p.iomuxc.gpio_ad_b1_00,
-                a0: p.iomuxc.gpio_ad_b1_10,
-                a1: p.iomuxc.gpio_ad_b1_11,
-                a2: p.iomuxc.gpio_ad_b1_04,
-                a3: p.iomuxc.gpio_ad_b1_05,
-                uart_tx: p.iomuxc.gpio_ad_b0_12.alt2(),
-                uart_rx: p.iomuxc.gpio_ad_b0_13.alt2(),
-            },
-            i2c: p.i2c,
-            spi: p.spi,
-            uart: p.uart,
-            gpr: p.iomuxc.gpr,
-        }
-    }
-}
-
-pub fn configure_led(
-    gpr: &mut hal::iomuxc::GPR,
-    mut pad: hal::iomuxc::gpio::GPIO_AD_B0_09<hal::iomuxc::Alt5>,
-) -> LED {
-    use hal::gpio::IntoGpio;
-    use hal::iomuxc::pin_config::*;
-    const led_pin_cfg: PinConfig = PinConfig::with_none()
-        .set_pull_up(PullUp::Keeper)
-        .set_speed(Speed::Speed2_150MHz)
-        .set_drive_strength(DriveStrength::R0_DIV_6);
-
-    pad.configure(&led_pin_cfg);
-    let mut pin = pad.into_gpio().output();
-    pin
-}
-
-/// Blocks for at least `millis` milliseconds
-///
-/// `delay()` will spin-loop on updates from SYSTICK, until
-/// `millis` milliseconds have elapsed. SYSTICK has a 1ms
-/// interrupt interval, so the minimal delay is around 1ms.
-#[no_mangle]
-pub extern "C" fn delay(millis: u32) {
-    if 0 == millis {
-        return;
-    }
-    let start = systick::read();
-    let target = start + millis;
-    loop {
-        let count = systick::read();
-        if count >= target {
-            return;
-        }
-    }
-}
-
-/// Scoping of data related to SYSTICK
-mod systick {
-    use imxrt_rt::exception;
-
-    #[no_mangle]
-    static mut systick_millis_count: u32 = 0;
-
-    #[exception]
-    fn SysTick() {
-        unsafe {
-            let ms = core::ptr::read_volatile(&systick_millis_count);
-            let ms = ms.wrapping_add(1);
-            core::ptr::write_volatile(&mut systick_millis_count, ms);
-        }
-    }
-
-    /// Read the systick counter. Returns an absolute value describing
-    /// the number of milliseconds since the SYSTICK handler was enabled.
-    /// This may be used to implement coarse timing.
-    pub fn read() -> u32 {
-        unsafe { core::ptr::read_volatile(&systick_millis_count) }
-    }
+/// Returns a GPIO that's physically tied to the LED. Use the returned handle
+/// to drive the LED.
+pub fn configure_led(mut pin: pins::D4) -> LED {
+    use hal::iomuxc::*;
+    const LED_PIN_CFG: Config = Config::modify()
+        .set_pull_keep_select(PullKeepSelect::Keeper)
+        .set_pull_keep(PullKeep::Disabled)
+        .set_speed(Speed::Fast)
+        .set_drive_strength(DriveStrength::R0_6);
+    configure(&mut pin, LED_PIN_CFG);
+    hal::gpio::GPIO::new(pin).output()
 }
